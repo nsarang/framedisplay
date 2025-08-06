df3f3161981f38d3dc2f0b812b17fed4c3382a47
Nima Sarang <nimasarang@gmail.com>
2025-08-06 16:22:04 -0700

    refactor: dynamically retrieve version from JavaScript file

--- framedisplay/__version__.py
+++ framedisplay/__version__.py
@@ -1,1 +1,6 @@
-__version__ = "1.1.3"
+import re
+from pathlib import Path
+
+version_file = Path(__file__).parent / "js/src/version.js"
+pattern = r"const\s+version\s*=\s*['\"]([^'\"]+)['\"]"
+__version__ = re.search(pattern, version_file.read_text()).group(1)
